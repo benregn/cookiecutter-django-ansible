03801846b3eaa904aed82d2695b7ade2b64b52dd
Tomas Thor Jonsson <benregn@gmail.com>
2014-06-14 21:30:42 +0200

    prevent debug_toolbar from patching the settings

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/settings.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/settings.py
@@ -294,6 +294,8 @@ class Local(Common):
     MIDDLEWARE_CLASSES = Common.MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
     INSTALLED_APPS += ('debug_toolbar',)
 
+    DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
     INTERNAL_IPS = ('127.0.0.1',)
 
     DEBUG_TOOLBAR_CONFIG = {
