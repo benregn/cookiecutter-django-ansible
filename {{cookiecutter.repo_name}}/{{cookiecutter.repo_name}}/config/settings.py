e9c3fc2120dade05baaf18649d256aefabc2aa5c
Tomas Thor Jonsson <benregn@gmail.com>
2014-09-09 06:39:51 +0200

    Revert "split settings into separate files"
    This reverts commit ccd6f6f16939a83a30da3cf22d20ed5469844ce1.

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/settings.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/config/settings.py
@@ -11,11 +11,19 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from os.path import join, dirname
+from os.path import join
+
+# See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
+try:
+    from S3 import CallingFormat
+    AWS_CALLING_FORMAT = CallingFormat.SUBDOMAIN
+except ImportError:
+    # TODO: Fix this where even if in Dev this class is called.
+    pass
 
 from configurations import Configuration, values
 
-BASE_DIR = dirname(dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 class Common(Configuration):
@@ -114,8 +122,8 @@ class Common(Configuration):
     # END DATABASE CONFIGURATION
 
     # CACHING
-    # Do this here because thanks to django-pylibmc-sasl and pylibmc
-    # memcacheify (used on heroku) is painful to install on windows.
+    # Do this here because thanks to django-pylibmc-sasl and pylibmc memcacheify is painful to install on windows.
+    # memcacheify is what's used in Production
     CACHES = {
         'default': {
             'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
@@ -202,10 +210,10 @@ class Common(Configuration):
     # END MEDIA CONFIGURATION
 
     # URL Configuration
-    ROOT_URLCONF = 'urls'
+    ROOT_URLCONF = 'config.urls'
 
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
-    WSGI_APPLICATION = 'wsgi.application'
+    WSGI_APPLICATION = 'config.wsgi.application'
     # End URL Configuration
 
     # AUTHENTICATION CONFIGURATION
@@ -263,8 +271,6 @@ class Common(Configuration):
     }
     # END LOGGING CONFIGURATION
 
-    # Your common stuff: Below this line define 3rd party libary settings
-
     # CELERY CONFIGURATION
     # See: http://docs.celeryproject.org/en/latest/configuration.html#broker-transport
     BROKER_TRANSPORT = 'amqp'
@@ -289,3 +295,138 @@ class Common(Configuration):
     # and: http://docs.celeryproject.org/en/latest/configuration.html#std:setting-CELERY_ACCEPT_CONTENT
     CELERY_ACCEPT_CONTENT = ['pickle', 'json']
     # END CELERY CONFIGURATION
+
+    # Your common stuff: Below this line define 3rd party libary settings
+
+
+class Local(Common):
+
+    # DEBUG
+    DEBUG = values.BooleanValue(True)
+    TEMPLATE_DEBUG = DEBUG
+    # END DEBUG
+
+    # INSTALLED_APPS
+    INSTALLED_APPS = Common.INSTALLED_APPS
+    # END INSTALLED_APPS
+
+    # Mail settings
+    EMAIL_HOST = "localhost"
+    EMAIL_PORT = 1025
+    EMAIL_BACKEND = values.Value('django.core.mail.backends.console.EmailBackend')
+    # End mail settings
+
+    # django-debug-toolbar
+    MIDDLEWARE_CLASSES = Common.MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    INSTALLED_APPS += ('debug_toolbar',)
+
+    DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+    INTERNAL_IPS = ('127.0.0.1',)
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'DISABLE_PANELS': [
+            'debug_toolbar.panels.redirects.RedirectsPanel',
+        ],
+        'SHOW_TEMPLATE_CONTEXT': True,
+    }
+    # end django-debug-toolbar
+
+    # Your local stuff: Below this line define 3rd party libary settings
+
+
+class Production(Common):
+
+    # INSTALLED_APPS
+    INSTALLED_APPS = Common.INSTALLED_APPS
+    # END INSTALLED_APPS
+
+    # SECRET KEY
+    SECRET_KEY = values.SecretValue()
+    # END SECRET KEY
+
+    # django-secure
+    INSTALLED_APPS += ("djangosecure", )
+
+    # set this to 60 seconds and then to 518400 when you can prove it works
+    SECURE_HSTS_SECONDS = 60
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = values.BooleanValue(True)
+    SECURE_FRAME_DENY = values.BooleanValue(True)
+    SECURE_CONTENT_TYPE_NOSNIFF = values.BooleanValue(True)
+    SECURE_BROWSER_XSS_FILTER = values.BooleanValue(True)
+    SESSION_COOKIE_SECURE = values.BooleanValue(False)
+    SESSION_COOKIE_HTTPONLY = values.BooleanValue(True)
+    SECURE_SSL_REDIRECT = values.BooleanValue(True)
+    # end django-secure
+
+    # SITE CONFIGURATION
+    # Hosts/domain names that are valid for this site
+    # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
+    ALLOWED_HOSTS = ["*"]
+    # END SITE CONFIGURATION
+
+    # STORAGE CONFIGURATION
+    # # See: http://django-storages.readthedocs.org/en/latest/index.html
+    # INSTALLED_APPS += (
+    #     'storages',
+    # )
+
+    # # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
+    # STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+    # # See: http://django-storages.readthedocs.org/en/latest/backends/amazon-S3.html#settings
+    # AWS_ACCESS_KEY_ID = values.SecretValue()
+    # AWS_SECRET_ACCESS_KEY = values.SecretValue()
+    # AWS_STORAGE_BUCKET_NAME = values.SecretValue()
+    # AWS_AUTO_CREATE_BUCKET = True
+    # AWS_QUERYSTRING_AUTH = False
+
+    # # see: https://github.com/antonagestam/collectfast
+    # AWS_PRELOAD_METADATA = True
+    # INSTALLED_APPS += ("collectfast", )
+
+    # # AWS cache settings, don't change unless you know what you're doing:
+    # AWS_EXPIREY = 60 * 60 * 24 * 7
+    # AWS_HEADERS = {
+    #     'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIREY,
+    #         AWS_EXPIREY)
+    # }
+
+    # # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+    # STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+    # END STORAGE CONFIGURATION
+
+    # EMAIL
+    DEFAULT_FROM_EMAIL = values.Value(
+        '{{cookiecutter.project_name}} <noreply@{{cookiecutter.domain_name}}>')
+    EMAIL_HOST = values.Value('smtp.sendgrid.com')
+    # EMAIL_HOST_PASSWORD = values.SecretValue(environ_prefix="", environ_name="SENDGRID_PASSWORD")
+    # EMAIL_HOST_USER = values.SecretValue(environ_prefix="", environ_name="SENDGRID_USERNAME")
+    EMAIL_PORT = values.IntegerValue(587, environ_prefix="", environ_name="EMAIL_PORT")
+    EMAIL_SUBJECT_PREFIX = values.Value('[{{cookiecutter.project_name}}] ', environ_name="EMAIL_SUBJECT_PREFIX")
+    EMAIL_USE_TLS = True
+    # SERVER_EMAIL = EMAIL_HOST_USER
+    # END EMAIL
+
+    # TEMPLATE CONFIGURATION
+
+    # See: https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
+    TEMPLATE_LOADERS = (
+        ('django.template.loaders.cached.Loader', (
+            'django.template.loaders.filesystem.Loader',
+            'django.template.loaders.app_directories.Loader',
+        )),
+    )
+    # END TEMPLATE CONFIGURATION
+
+    # CACHING
+    # Only do this here because thanks to django-pylibmc-sasl and pylibmc memcacheify is painful to install on windows.
+    try:
+        # See: https://github.com/rdegges/django-heroku-memcacheify
+        from memcacheify import memcacheify
+        CACHES = memcacheify()
+    except ImportError:
+        CACHES = values.CacheURLValue(default="memcached://127.0.0.1:11211")
+    # END CACHING
+
+    # Your production stuff: Below this line define 3rd party libary settings
